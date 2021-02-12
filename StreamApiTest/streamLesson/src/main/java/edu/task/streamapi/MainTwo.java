2d0692ace74a8da4f82b905f15f6d84a67d7966e
Anton Lysenko <anton.lys96@gmail.com>
2021-02-12 02:26:45 +0200

    streamApi

--- StreamApiTest/streamLesson/src/main/java/edu/task/streamapi/MainTwo.java
+++ StreamApiTest/streamLesson/src/main/java/edu/task/streamapi/MainTwo.java
@@ -5,23 +5,22 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import edu.task.behaviour.StringFormatter;
 import static java.lang.System.lineSeparator;
-import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.*;
 
 public class MainTwo {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println(read());
+		read();
 	}
 
 	public static String read() throws IOException {
 		File file = new File(
 				"/home/gweep/Documents/GitHub/SimpleExamples/StreamApiTest/streamLesson/src/main/resources/file.txt");
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			List<String> strings = reader.lines().collect(Collectors.toList());
+			List<String> strings = reader.lines().collect(toList());
 			return formatter.listToString(strings);
 		}
 
@@ -29,8 +28,12 @@ public class MainTwo {
 
 	public static StringFormatter formatter = (strings) -> {
 		return strings.stream().map(s -> {
-			String text = "";
-			return text += s;
+			System.out.println(strings.indexOf(s) + ": " + s);  /* 0: 1.txt 
+																 * 1: file.txt 
+																 * 2: x 
+																 * 3: z 
+																 * 4: t */
+			return s;
 		}).collect(joining(lineSeparator()));
 	};
 }
