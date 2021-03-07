04c9763b91f6e903216291c61376176a93a6fa96
Anton Lysenko <anton.lys96@gmail.com>
2021-03-07 06:27:40 +0200

    streams

--- StreamApiTest/streamLesson/src/main/java/edu/task/streamapi/MainStreamTest.java
+++ StreamApiTest/streamLesson/src/main/java/edu/task/streamapi/MainStreamTest.java
@@ -2,9 +2,11 @@ package edu.task.streamapi;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
@@ -33,6 +35,29 @@ public class MainStreamTest {
 		out.println(getModelsLength(cars)); // [4, 5, 5, 6]
 		out.println(getModelsUniqueChar(cars)); // [A, u, d, i, L, a, m, b, a, T, e, s, l, a, P, o, r, s, h, e]
 		out.println(getModelsUniqueCharArray(cars)); // [A, u, d, i, L, a, m, b, T, e, s, l, P, o, r, h]
+		out.println(getNewNumbers(getModelsLength(cars))); // [5, 7, 7, 7]
+		// out.println(getNewList(getModelsLength(cars),
+
+		for (Integer[] i : getNewList(getModelsLength(cars), getNewNumbers(getModelsLength(cars)))) {
+			out.println(Arrays.toString(i)); // [4, 5]
+			// [4, 7]
+			// [4, 7]
+			// [4, 7]
+			// [5, 5]
+			// [5, 7]
+			// [5, 7]
+			// [5, 7]
+			// [5, 5]
+			// [5, 7]
+			// [5, 7]
+			// [5, 7]
+			// [6, 5]
+			// [6, 7]
+			// [6, 7]
+			// [6, 7]
+		}
+		out.println(getNewListTwo(getModelsLength(cars), getNewNumbers(getModelsLength(cars)))); // [4, 5, 7, 6]
+
 	}
 
 	public static void main(String[] args) {
@@ -85,6 +110,37 @@ public class MainStreamTest {
 		return cars.stream().map(car -> {
 			return car.getModel().split("");
 		}).flatMap(Arrays::stream).distinct().collect(toList());
+	}
+
+	List<Integer> getNewNumbers(List<Integer> digits) {
+		return digits.stream().map(number -> {
+			if (number % 2 == 0) {
+				return number + 1;
+			} else if (number % 2 != 0) {
+				return number + 2;
+			}
+			return 0;
+		}).collect(toList());
+	}
+
+	List<Integer[]> getNewList(List<Integer> l1, List<Integer> l2) {
+		return l1.stream().flatMap(i -> l2.stream().map(j -> new Integer[] { i, j })).collect(toList());
+	}
+
+	List<Integer> getNewListTwo(List<Integer> l1, List<Integer> l2) {
+		List<Integer> arrInt = new ArrayList<>();
+		Integer[] array = new Integer[100];
+
+		return l1.stream().flatMap(i -> l2.stream().map(j -> {
+			arrInt.add(i);
+			arrInt.add(j);
+			return arrInt;
+		})).map(arr -> {
+			for (int x = 0; x < arr.size(); x++) {
+				array[x] = arr.get(x);
+			}
+			return array;
+		}).flatMap(Arrays::stream).filter(Objects::nonNull).distinct().collect(toList());
 	}
 
 }
