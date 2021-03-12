35f0a8df41205a87d5aa28d0c46486d31be724ff
Anton Lysenko <anton.lys96@gmail.com>
2021-03-12 22:09:14 +0200

    tasksAlgo

--- TasksAlgorithm/src/main/java/edu/tasks/leetcodeEasyTask/Solution4.java
+++ TasksAlgorithm/src/main/java/edu/tasks/leetcodeEasyTask/Solution4.java
@@ -1,9 +1,11 @@
 package edu.tasks.leetcodeEasyTask;
 
+import java.util.Arrays;
+
 public class Solution4 {
 
 	Solution4() {
-		System.out.println(shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3));
+		System.out.println(Arrays.toString(shuffle(new int[] { 1, 1, 2, 2 }, 2)));// [1, 2, 1, 2]
 	}
 
 	public static void main(String[] args) {
@@ -13,10 +15,27 @@ public class Solution4 {
 	public int[] shuffle(int[] nums, int n) {
 		int odd[] = new int[n];
 		int even[] = new int[n];
-		for(int i = 0; i < nums.length; i++) {
-			
+		int arr[] = new int[n * 2];
+		int p = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (i < nums.length / 2) {
+				odd[i] = nums[i];
+			} else if (i >= nums.length / 2) {
+				even[p] = nums[i];
+				p++;
+			}
 		}
-		
-		return new int[] {};
+		int z = 0;
+		int x = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				arr[i] = odd[z];
+				z++;
+			} else {
+				arr[i] = even[x];
+				x++;
+			}
+		}
+		return arr;
 	}
 }
