2c1ba6122352c4170c8a1105967b44f020284e78
Anton Lysenko <anton.lys96@gmail.com>
2021-04-11 07:12:07 +0300

    mapFromLists

--- TasksAlgorithm/src/main/java/edu/tasks/algo/Main1.java
+++ TasksAlgorithm/src/main/java/edu/tasks/algo/Main1.java
@@ -12,8 +12,8 @@ private String GET_ALL_LESSON_GROUP = "SELECT * FROM lessons_groups";
 				Statement statement = connection.createStatement();) {
 			ResultSet resultSet = statement.executeQuery(GET_ALL_LESSON_GROUP);
 			while (resultSet.next()) {
-				lessonIds.add(resultSet.getInt("column_id"));
-				groupIds.add(resultSet.getInt("column2_id"));
+				lessonIds.add(resultSet.getInt("lesson_id"));
+				groupIds.add(resultSet.getInt("group_id"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -25,23 +25,31 @@ private String GET_ALL_LESSON_GROUP = "SELECT * FROM lessons_groups";
 		List<Integer> lengths = new ArrayList<>();
 		int counterRepeat = 0;
 		List<Integer> listKeys = new ArrayList<>(setKeys);
+		List<Integer> indexes = new ArrayList<>();
 		for (int keyIndex = 0; keyIndex < listKeys.size(); keyIndex++) {
 			for (int lessonIndex = 0; lessonIndex < lessonIds.size(); lessonIndex++) {
 				if (listKeys.get(keyIndex).equals(lessonIds.get(lessonIndex))) {
 					counterRepeat++;
+					indexes.add(lessonIndex);
 				}
 			}
 			lengths.add(counterRepeat);
 			counterRepeat = 0;
 		}
+		List<Integer> sortedLessonIds = new ArrayList<>();
+		List<Integer> sortedGroupIds = new ArrayList<>();
+		for (Integer id : indexes) {
+			sortedLessonIds.add(lessonIds.get(id));
+			sortedGroupIds.add(groupIds.get(id));
+		}
 		int increment = 0;
 		for (int indexKey = 0; indexKey < lengths.size(); indexKey++) {
 			List<Integer> values = new ArrayList<>();
 			for (int indexValue = increment; indexValue < increment + lengths.get(indexKey); indexValue++) {
-				values.add(groupIds.get(indexValue));
+				values.add(sortedGroupIds.get(indexValue));
 			}
 			increment += lengths.get(indexKey);
-			map.put(indexKey + 1, values);
+			map.put(listKeys.get(indexKey), values);
 		}
 		return map;
 	}
