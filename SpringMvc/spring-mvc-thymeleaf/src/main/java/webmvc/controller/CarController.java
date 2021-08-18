1eb7137e819dd80bfd1c91e0ffa820801d7d5c7b
Anton Lysenko <anton.lys96@gmail.com>
2021-08-18 23:02:18 +0300

    addIndexPage

--- SpringMvc/spring-mvc-thymeleaf/src/main/java/webmvc/controller/CarController.java
+++ SpringMvc/spring-mvc-thymeleaf/src/main/java/webmvc/controller/CarController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import webmvc.dao.CarDao;
 
 @Controller
-@RequestMapping("/cars")
 public class CarController {
 
 	private CarDao carDao;
@@ -17,7 +15,12 @@ public class CarController {
 	public CarController(CarDao carDao) {
 		this.carDao = carDao;
 	}
-	
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
 	@GetMapping("/findCars")
 	public String findCars(Model model) {
 		model.addAttribute("cars", carDao.getAll());
