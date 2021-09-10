59c8bd070471af6397cc03deb8b4da19416acffc
user <anton.lys96@gmail.com>
2021-09-10 19:30:00 +0300

    easy

--- leetcode/easy/src/easy/task2/Solution.java
+++ leetcode/easy/src/easy/task2/Solution.java
@@ -7,34 +7,46 @@ import java.util.List;
 public class Solution {
 
 	public int reverse(int x) {
+		int z = 0;
+		boolean f = false;
+		if (x == 0) {
+			return z;
+		} else if (x < 0) {
+			x = x * (-1);
+			f = true;
+		}
 		List<Integer> lInt = new ArrayList<>();
 		List<Integer> l2 = new ArrayList<>();
+
 		int count = 0;
-		int c = 0 ;
+		int c = 0;
 		while (x > 0) {
 			c = x % 10;
-			System.out.println(c);
 			lInt.add(c);
 			x = x / 10;
 			count++;
 		}
+
 		int p = 1;
-		for(int i = 1; i < count; i++) {
+		for (int i = 1; i < count; i++) {
 			p *= 10;
 			l2.add(p);
 		}
 		Collections.reverse(l2);
-		int z = 0;
-		for(int i = 0; i < l2.size(); i++) {
+
+		for (int i = 0; i < l2.size(); i++) {
 			z += lInt.get(i) * l2.get(i);
 		}
 		z += lInt.get(lInt.size()-1);
-		System.out.println(z);
+		if(f == true) {
+			z = z * (-1);
+			return z;
+		}
 		return z;
 	}
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		solution.reverse(1245);
+		 System.out.println( solution.reverse(-12));
 	}
 }
