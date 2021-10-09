a8b5c1fa32ceb51444c9c1804a1bd52c8def393b
Anton Lysenko <anton.lys96@gmail.com>
2021-10-09 17:01:33 +0300

    task

--- TrackEnsure/junit/src/test/java/net/gweep/junit/MainTest.java
+++ TrackEnsure/junit/src/test/java/net/gweep/junit/MainTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,36 +13,40 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
 	private Main main;
+	private List<Directory> expected;
+	private List<Directory> actual;
+	private Directory root;
 
 	@BeforeEach
 	void setUp() {
 		main = new Main();
-	}
-
-	@Test
-	void traverseInOrderTest() {
 		File file = new File("fileOne", LocalDateTime.now().minusMinutes(77));
 		Directory subDir = new Directory("dirTwo", new Directory(), List.of(),
 				List.of(new File("fileTwo", LocalDateTime.now())));
-		Directory root = new Directory("dirOne", subDir, List.of(), List.of(file));
-		List<Directory> expected = new ArrayList<>();
+		List<File> files = new ArrayList<>();
+		files.add(file);
+		root = new Directory("dirOne", subDir, List.of(), files);
+		expected = new ArrayList<>();
 		expected.add(root);
 		expected.add(subDir);
-		assertEquals(expected, main.traverseInOrder(root, new ArrayList<>()));
-		List<File> filesExpected = new ArrayList<>();
-		List<File> filesActual = new ArrayList<>();
-		for (Directory dir : expected) {
-			for (File f : dir.getFiles()) {
-				filesExpected.add(f);
-			}
-		}
-		for (Directory dir : main.traverseInOrder(root, new ArrayList<>())) {
-			for (File f : dir.getFiles()) {
-				filesActual.add(f);
-			}
-		}
-		assertEquals(filesExpected, filesActual);
-
+		actual = main.traverseInOrder(root, new ArrayList<>());
 	}
 
+	@Test
+	void traverseInOrderDirTest() {
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void traverseInOrderFileTest() {
+		List<File> expectedFiles = expected.stream().map(d -> {
+			int count = 0;
+			return d.getFiles().get(count++);
+		}).collect(Collectors.toList());
+		List<File> actualFiles = actual.stream().map(d -> {
+			int count = 0;
+			return d.getFiles().get(count++);
+		}).collect(Collectors.toList());
+		assertEquals(expectedFiles, actualFiles);
+	}
 }
