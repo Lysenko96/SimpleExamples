0d2aa459a957cb24136f3f36d8656f8de617a5b0
Anton Lysenko <anton.lys96@gmail.com>
2021-11-16 06:03:51 +0200

    sql

---
+++ TrackEnsure/sql/task2.sql
@@ -0,0 +1,39 @@
+
+-- Provide a query showing Customers (just their full names, customer ID and country) who are not in the US
+SELECT "CustomerId", "FirstName", "LastName", "Country" FROM "Customer" WHERE "Country" != 'USA';
+
+-- Provide a query only showing the Customers from Brazil
+SELECT * FROM "Customer" WHERE "Country" = 'Brazil';
+
+-- Provide a query showing the Invoices of customers who are from Brazil. 
+-- The resultant table should show the customer’s full name, Invoice ID, Date of the invoice and billing country.
+SELECT "Customer"."FirstName", "Customer"."LastName", "Invoice"."InvoiceId", "Invoice"."InvoiceDate", "Invoice"."BillingCountry"  FROM "Invoice", "Customer" WHERE "Customer"."Country" = 'Brazil';
+
+-- Provide a query showing only the Employees who are Sales Agents.
+SELECT * FROM "Employee" WHERE "Title"='Sales Support Agent';
+
+-- Provide a query showing a unique list of billing countries from the Invoice table
+SELECT DISTINCT "Invoice"."BillingCountry" FROM "Invoice";
+
+-- Provide a query that shows the invoices associated with each sales agent.
+-- The resultant table should include the Sales Agent’s full name.
+SELECT "Employee"."LastName", "Employee"."FirstName", "Invoice".* FROM "Employee", "Invoice" WHERE "Title"='Sales Support Agent';
+
+-- Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers
+SELECT DISTINCT "Invoice"."Total", "Customer"."FirstName", "Customer"."Country", "Employee"."FirstName" FROM "Invoice", "Customer", "Employee" WHERE "Employee"."Title"='Sales Support Agent';
+
+-- How many Invoices were there in 2009 and 2011? 
+SELECT * FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2011-01-01';
+
+-- What are the respective total sales for each of those years?
+SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12';
+SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2010-01-01' AND '2010-12-12';
+SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2011-01-01' AND '2011-12-12';
+
+
+-- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37
+SELECT COUNT("InvoiceLine") FROM "InvoiceLine" WHERE "InvoiceId"=37;
+
+-- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+SELECT COUNT("InvoiceId") FROM "InvoiceLine" GROUP BY "InvoiceId";
+
