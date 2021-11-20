a24c91cf64d54472795d0b1d62fa44b6b760b95b
Anton Lysenko <anton.lys96@gmail.com>
2021-11-20 13:19:34 +0200

    sql

--- TrackEnsure/sql/task2.sql
+++ TrackEnsure/sql/task2.sql
@@ -1,45 +1,83 @@
 
 -- Provide a query showing Customers (just their full names, customer ID and country) who are not in the US
-SELECT "CustomerId", "FirstName", "LastName", "Country" FROM "Customer" WHERE "Country" != 'USA';
+SELECT "CustomerId", CONCAT("FirstName", ' ', "LastName") AS "Full name", "Country" 
+FROM "Customer" 
+WHERE "Country" != 'USA';
 
 -- Provide a query only showing the Customers from Brazil
-SELECT * FROM "Customer" WHERE "Country" = 'Brazil';
+SELECT * 
+FROM "Customer" 
+WHERE "Country" = 'Brazil';
 
 -- Provide a query showing the Invoices of customers who are from Brazil. 
 -- The resultant table should show the customer’s full name, Invoice ID, Date of the invoice and billing country.
-SELECT "Customer"."FirstName", "Customer"."LastName", "Invoice"."InvoiceId", "Invoice"."InvoiceDate", "Invoice"."BillingCountry"  FROM "Invoice" INNER JOIN "Customer" ON "Invoice"."CustomerId"="Customer"."CustomerId" WHERE "Customer"."Country" = 'Brazil';
+SELECT  CONCAT("FirstName",  ' ', "LastName") AS "Full name",  "Invoice"."InvoiceId", "Invoice"."InvoiceDate", "Invoice"."BillingCountry"  
+FROM "Invoice" 
+LEFT JOIN "Customer" 
+ON "Invoice"."CustomerId"="Customer"."CustomerId" 
+WHERE "Customer"."Country" = 'Brazil';
 
 -- Provide a query showing only the Employees who are Sales Agents.
-SELECT * FROM "Employee" WHERE "Title"='Sales Support Agent';
+SELECT * 
+FROM "Employee" 
+WHERE "Title"='Sales Support Agent';
 
 -- Provide a query showing a unique list of billing countries from the Invoice table
-SELECT DISTINCT "Invoice"."BillingCountry" FROM "Invoice";
+SELECT DISTINCT "Invoice"."BillingCountry" 
+FROM "Invoice";
 
 -- Provide a query that shows the invoices associated with each sales agent.
 -- The resultant table should include the Sales Agent’s full name.
-
-SELECT "Invoice".*, "Employee"."FirstName", "Employee"."LastName" FROM "Invoice" INNER JOIN "Customer" ON "Invoice"."CustomerId"="Customer"."CustomerId" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId";
-
-
+SELECT "Invoice".*, CONCAT("Employee"."FirstName", ' ', "Employee"."LastName") AS "Full name" 
+FROM "Invoice" 
+LEFT JOIN "Customer" 
+ON "Invoice"."CustomerId"="Customer"."CustomerId" 
+LEFT JOIN "Employee" 
+ON "Customer"."SupportRepId"="Employee"."EmployeeId";
 
 -- Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers
-SELECT DISTINCT  "Invoice"."Total", "Customer"."FirstName", "Customer"."Country", "Employee"."FirstName" FROM "Invoice" INNER JOIN "Customer" ON "Invoice"."CustomerId"="Customer"."CustomerId" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId";
+SELECT DISTINCT  "Invoice"."Total",  "Customer"."FirstName", "Customer"."Country", "Employee"."FirstName" 
+FROM "Invoice" 
+LEFT JOIN "Customer" 
+ON "Invoice"."CustomerId"="Customer"."CustomerId" 
+LEFT JOIN "Employee" 
+ON "Customer"."SupportRepId"="Employee"."EmployeeId";
 
 
 -- How many Invoices were there in 2009 and 2011? 
-SELECT * FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2011-12-12';
+SELECT * 
+FROM "Invoice" 
+WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-31'
+UNION (
+SELECT * 
+FROM "Invoice" 
+WHERE "InvoiceDate" BETWEEN '2011-01-01' AND '2011-12-31');
 
 -- What are the respective total sales for each of those years?
-SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12';
-SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2010-01-01' AND '2010-12-12';
-SELECT SUM("Total") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2011-01-01' AND '2011-12-12';
+SELECT SUM("Total")
+FROM "Invoice" 
+WHERE "InvoiceDate" 
+BETWEEN '2009-01-01' AND '2009-12-31'
+UNION(
+SELECT SUM("Total")
+FROM "Invoice" 
+WHERE "InvoiceDate" 
+BETWEEN '2011-01-01' AND '2011-12-31');
 
-SELECT SUM("Total"), EXTRACT(YEAR FROM "InvoiceDate") FROM "Invoice" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2011-12-12' GROUP BY EXTRACT(YEAR FROM "InvoiceDate");
+SELECT SUM("Total"), EXTRACT(YEAR FROM "InvoiceDate") AS year
+FROM "Invoice" 
+WHERE "InvoiceDate"
+BETWEEN '2009-01-01' AND '2011-12-31' AND EXTRACT(YEAR FROM "InvoiceDate") != '2010'
+GROUP BY year;
 
 
 -- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37
-SELECT COUNT("InvoiceLine") FROM "InvoiceLine" WHERE "InvoiceId"=37;
+SELECT COUNT("InvoiceLine") 
+FROM "InvoiceLine" 
+WHERE "InvoiceId"=37;
 
 -- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
-SELECT "InvoiceId", COUNT("InvoiceId") FROM "InvoiceLine" GROUP BY "InvoiceId";
+SELECT "InvoiceId", COUNT("InvoiceId") 
+FROM "InvoiceLine" 
+GROUP BY "InvoiceId";
 
--- TrackEnsure/sql/task3.sql
+++ TrackEnsure/sql/task3.sql
@@ -1,8 +1,18 @@
 -- Provide a query that includes the track name with each invoice line item
-SELECT "Track"."Name", "InvoiceLine".* FROM "InvoiceLine" INNER JOIN "Track" ON "InvoiceLine"."TrackId"="Track"."TrackId";
+SELECT "Track"."Name", "InvoiceLine".* 
+FROM "InvoiceLine" 
+INNER JOIN "Track" 
+ON "InvoiceLine"."TrackId"="Track"."TrackId";
 
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
-SELECT "InvoiceLine".*, "Track"."Name", "Artist"."Name"  FROM "InvoiceLine" INNER JOIN "Track" ON "InvoiceLine"."TrackId"="Track"."TrackId" INNER JOIN "Album" ON "Track"."AlbumId"="Album"."AlbumId" INNER JOIN "Artist" ON "Album"."ArtistId"="Artist"."ArtistId";
+SELECT "InvoiceLine".*, "Track"."Name", "Artist"."Name"  
+FROM "InvoiceLine" 
+INNER JOIN "Track" 
+ON "InvoiceLine"."TrackId"="Track"."TrackId" 
+INNER JOIN "Album" 
+ON "Track"."AlbumId"="Album"."AlbumId" 
+INNER JOIN "Artist" 
+ON "Album"."ArtistId"="Artist"."ArtistId";
 
 
 -- Provide a query that shows the # of invoices per country. HINT: GROUP BY
@@ -10,47 +20,115 @@ SELECT COUNT("InvoiceId"), "BillingCountry" FROM "Invoice" GROUP BY "BillingCoun
 
 -- Provide a query that shows the total number of tracks in each playlist. 
 -- The Playlist name should be included on the resultant table
-SELECT "Playlist"."Name", COUNT("PlaylistTrack"."PlaylistId") FROM "PlaylistTrack" INNER JOIN "Playlist" ON "PlaylistTrack"."PlaylistId"="Playlist"."PlaylistId"  GROUP BY "PlaylistTrack"."PlaylistId", "Playlist"."Name";
+SELECT "Playlist"."Name", COUNT("PlaylistTrack"."PlaylistId") 
+FROM "Playlist" 
+LEFT JOIN "PlaylistTrack" 
+ON "PlaylistTrack"."PlaylistId"="Playlist"."PlaylistId"  
+GROUP BY "Playlist"."PlaylistId", "Playlist"."Name";
+
+-- select p2.playlistid, p2.name playlist, count(t.trackid) tracks 
+--from playlist p2 
+--left join playlisttrack p 
+--on p2.playlistid = p.playlistid 
+--left join track t on t.trackid = p.trackid group by p2.playlistid, p2.name order by 1 
+
 
 -- Provide a query that shows all the Tracks, but displays no IDs. 
 -- The resultant table should include the Album name, Media type and Genre
-SELECT DISTINCT "Track"."Name", "Album"."Title", "MediaType"."Name", "Genre"."Name" FROM "Track" INNER JOIN "Album" ON "Track"."AlbumId"="Album"."AlbumId" INNER JOIN "MediaType" ON "Track"."MediaTypeId"="MediaType"."MediaTypeId" INNER JOIN "Genre" ON "Track"."GenreId"="Genre"."GenreId";
+SELECT "Track"."Name", "Track"."Composer", "Track"."Milliseconds", "Track"."Bytes", "Track"."UnitPrice", "Album"."Title", "MediaType"."Name", "Genre"."Name" 
+FROM "Track" 
+LEFT JOIN "Album" 
+ON "Track"."AlbumId"="Album"."AlbumId" 
+LEFT JOIN "MediaType" 
+ON "Track"."MediaTypeId"="MediaType"."MediaTypeId" 
+LEFT JOIN "Genre" 
+ON "Track"."GenreId"="Genre"."GenreId";
 
--- Provide a query that shows all Invoices but includes the # of invoice line items
-SELECT "InvoiceLine"."InvoiceLineId", "Invoice".* FROM "InvoiceLine" INNER JOIN "Invoice" ON "InvoiceLine"."InvoiceId"="Invoice"."InvoiceId";
+
+-- Provide a query that shows all Invoices but includes the # of invoice line items 
+SELECT "Invoice".*, COUNT("InvoiceLine"."InvoiceId")
+FROM "InvoiceLine" 
+INNER JOIN "Invoice"
+ON "InvoiceLine"."InvoiceId"="Invoice"."InvoiceId"
+GROUP BY "InvoiceLine"."InvoiceId", "Invoice"."InvoiceId";
 
 -- Provide a query that shows total sales made by each sales agent.
---SELECT COUNT("Customer"."SupportRepId"), "Employee"."FirstName", "Employee"."LastName" FROM "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" GROUP BY "Customer"."SupportRepId", "Employee"."FirstName", "Employee"."LastName";
 
-SELECT "Customer"."SupportRepId", COUNT("Customer"."SupportRepId") FROM "Invoice" INNER JOIN "Customer" ON "Invoice"."CustomerId"="Customer"."CustomerId" GROUP BY "Customer"."SupportRepId";
+--SELECT COUNT("Customer"."SupportRepId"), "Employee"."FirstName", "Employee"."LastName" 
+--FROM "Customer" 
+--INNER JOIN "Employee" 
+--ON "Customer"."SupportRepId"="Employee"."EmployeeId" 
+--GROUP BY "Customer"."SupportRepId", "Employee"."FirstName", "Employee"."LastName";
+
+SELECT SUM("Invoice"."Total")
+FROM "Invoice"
+INNER JOIN "Customer"
+ON "Invoice"."CustomerId"="Customer"."CustomerId"
+INNER JOIN "Employee"
+ON "Customer"."SupportRepId"="Employee"."EmployeeId"
+GROUP BY "Employee"."EmployeeId";
 
 
 -- Which sales agent made the most in sales in 2009?
  
 --SELECT DISTINCT "Customer"."SupportRepId", SUM("Invoice"."Total") FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12' GROUP BY "Customer"."SupportRepId", "Invoice"."InvoiceId", "Employee"."EmployeeId";
 
-SELECT "Customer"."SupportRepId", SUM("Invoice"."Total") FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12' GROUP BY "Customer"."SupportRepId", "Employee"."EmployeeId";
+SELECT "Employee"."EmployeeId", SUM("Invoice"."Total") AS total
+FROM "Invoice"
+INNER JOIN "Customer"
+ON "Invoice"."CustomerId"="Customer"."CustomerId"
+INNER JOIN "Employee" 
+ON "Customer"."SupportRepId"="Employee"."EmployeeId" 
+WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-31' 
+GROUP BY "Employee"."EmployeeId"
+ORDER BY total DESC
+LIMIT 1;
 
 
 -- Which sales agent made the most in sales in 2010?
 
 --SELECT DISTINCT "Customer"."SupportRepId", SUM("Invoice"."Total") FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2010-01-01' AND '2010-12-12' GROUP BY "Customer"."SupportRepId", "Invoice"."InvoiceId", "Employee"."EmployeeId";
 
-SELECT "Customer"."SupportRepId", SUM("Invoice"."Total") FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12' GROUP BY "Customer"."SupportRepId", "Employee"."EmployeeId";
+--SELECT "Customer"."SupportRepId", SUM("Invoice"."Total") FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12' GROUP BY "Customer"."SupportRepId", "Employee"."EmployeeId";
+
+SELECT "Employee"."EmployeeId", SUM("Invoice"."Total") AS total
+FROM "Invoice"
+INNER JOIN "Customer"
+ON "Invoice"."CustomerId"="Customer"."CustomerId"
+INNER JOIN "Employee" 
+ON "Customer"."SupportRepId"="Employee"."EmployeeId" 
+WHERE "InvoiceDate" BETWEEN '2010-01-01' AND '2010-12-31' 
+GROUP BY "Employee"."EmployeeId"
+ORDER BY total DESC
+LIMIT 1;
 
 -- test max
 
-SELECT MAX(sumTotal) FROM(SELECT "Employee"."EmployeeId", SUM("Invoice"."Total") AS sumTotal FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" WHERE "InvoiceDate" BETWEEN '2009-01-01' AND '2009-12-12' GROUP BY "Employee"."EmployeeId") query ;
+--SELECT MAX(sumTotal) 
+--FROM(
+--SELECT "Employee"."EmployeeId", SUM("Invoice"."Total") AS sumTotal 
+--FROM "Invoice", "Customer" 
+--INNER JOIN "Employee" 
+--ON "Customer"."SupportRepId"="Employee"."EmployeeId" 
+--WHERE "InvoiceDate" 
+--BETWEEN '2009-01-01' AND '2009-12-12' 
+--GROUP BY "Employee"."EmployeeId") query ;
 
 
 -- Which sales agent made the most in sales over all?
 
+SELECT "Employee"."EmployeeId", SUM("Invoice"."Total") AS total
+FROM "Invoice"
+INNER JOIN "Customer"
+ON "Invoice"."CustomerId"="Customer"."CustomerId"
+INNER JOIN "Employee" 
+ON "Customer"."SupportRepId"="Employee"."EmployeeId" 
+GROUP BY "Employee"."EmployeeId"
+ORDER BY total DESC
+LIMIT 1;
+
 -- SELECT "Customer"."SupportRepId", SUM("Invoice"."Total")  FROM "Invoice", "Customer" INNER JOIN "Employee" ON "Customer"."SupportRepId"="Employee"."EmployeeId" GROUP BY "Customer"."SupportRepId", "Employee"."EmployeeId";
 
-SELECT "Customer"."SupportRepId", SUM("Invoice"."Total")  FROM "Invoice" INNER JOIN "Customer" ON "Invoice"."CustomerId"="Customer"."CustomerId" GROUP BY "Customer"."SupportRepId";
-
-
-
-
-
-
+--SELECT "Customer"."SupportRepId", SUM("Invoice"."Total")  
+--FROM "Invoice" INNER JOIN "Customer" 
+--ON "Invoice"."CustomerId"="Customer"."CustomerId" GROUP BY "Customer"."SupportRepId";
