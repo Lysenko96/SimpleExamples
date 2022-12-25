7ea82afbab7b846211057828bfb72dda52ef2341
Anton Lysenko <anton.lys96@gmail.com>
2022-12-25 19:28:41 +0200

    template

---
+++ headfirst/src/info/lysenko/anton/patterns/template1/Duck.java
@@ -0,0 +1,41 @@
+package info.lysenko.anton.patterns.template1;
+
+public class Duck implements Comparable<Duck>{
+
+    private String name;
+    private int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Duck duck) {
+        return this.weight - duck.getWeight();
+    }
+}
