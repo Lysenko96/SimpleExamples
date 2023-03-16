593eddd97d5b901327b0c719ba919042010464aa
Anton Lysenko <anton.lys96@gmail.com>
2023-03-16 18:08:48 +0200

    codewars

--- untitled/src/collection/Main.java
+++ untitled/src/collection/Main.java
@@ -1,10 +1,6 @@
 package collection;
 
 import java.util.*;
-import java.util.function.ToIntFunction;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -12,6 +8,69 @@ public class Main {
         System.out.println(isIsogram(""));
         System.out.println(new Main().findNextSquare(121));
         System.out.println(new Main().sum(Arrays.asList(1,5,"3","2")));
+        List<Integer> l = new ArrayList<>(3);
+        l.add(1);
+        l.add(3);
+        l.add(4);
+        Collections.fill(l, 2);
+        Collections.addAll(l, Arrays.asList(3,5,1,5).toArray(new Integer[4]));
+        System.out.println(Collections.frequency(l, 2));
+        System.out.println(Collections.disjoint(Collections.singletonList(2), Collections.singletonList(2)));
+        System.out.println(l);
+        Stack<Integer> s = new Stack<>();
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        s.push(1);
+        System.out.println(s);
+        System.out.println(s.pop());
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        queue.poll();
+        System.out.println(queue);
+        print(new String[]{"str", "arb"});
+        print(new Integer[]{32,235});
+        System.out.println(disemvowel("LOL"));
+    }
+
+    public static String disemvowel(String str) {
+        List<String> exclude = new ArrayList<>(Arrays.asList("A","O","E","U","I","a","o","e","u","i"));
+        List<String> letters = new ArrayList<>(Arrays.asList(str.split("")));
+        StringBuilder sb = new StringBuilder();
+        for(String letter : letters){
+            if(!exclude.contains(letter)){
+                sb.append(letter);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static Integer basicMath(String op, int v1, int v2) {
+        int result = 0;
+        switch (op) {
+            case "+": result = v1 + v2;
+                break;
+            case "-": result = v1 - v2;
+                break;
+            case "*": result = v1 * v2;
+                break;
+            case "/": result = v1 / v2;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+    public static <T extends Comparable<T>> void print(T[] arr){
+        for(T elem : arr)
+            System.out.println(elem);
+
+        System.out.println();
     }
 
     public static boolean  isIsogram(String str) {
