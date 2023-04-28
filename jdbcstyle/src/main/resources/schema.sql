8d4848c900c9d458d7adfa2249a5aefb2ab4f905
Anton Lysenko <anton.lys96@gmail.com>
2023-04-28 08:36:30 -0400

    statementAddBatch

--- jdbcstyle/src/main/resources/schema.sql
+++ jdbcstyle/src/main/resources/schema.sql
@@ -1,13 +1,29 @@
 drop table if exists person;
 
-create table person(
-id serial primary key,
-name text,
-surname text,
-sex text,
-email text,
-year int,
-address text,
-phone int,
-"postCode" text
+create table person
+(
+    id         serial primary key,
+    name       text,
+    surname    text,
+    sex        text,
+    email      text,
+    year       int,
+    address    text,
+    phone      int,
+    "postCode" text
+);
+
+drop table if exists person_test;
+
+create table person_test
+(
+    id         serial primary key,
+    name       text,
+    surname    text,
+    sex        text,
+    email      text,
+    year       int,
+    address    text,
+    phone      int,
+    "postCode" text
 );
