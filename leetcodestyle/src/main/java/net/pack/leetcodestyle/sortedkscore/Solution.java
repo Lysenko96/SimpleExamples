03ee2810c8bdc9166bebb267b14a2c75bc90706e
Anton Lysenko <anton.lys96@gmail.com>
2023-10-07 23:45:53 +0300

    leetmedium

--- leetcodestyle/src/main/java/net/pack/leetcodestyle/sortedkscore/Solution.java
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/sortedkscore/Solution.java
@@ -1,28 +1,36 @@
 package net.pack.leetcodestyle.sortedkscore;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-//    public static void main(String[] args) {
-//        new Solution().sortTheStudents(new int[][]{{3,4},{5,6}}, 0);
-//    }
-//
-//    public int[][] sortTheStudents(int[][] score, int k) {
-//        Map<Integer, List<Integer>> table = new HashMap<>();
-//        for(int i = 0; i < score.length; i++){
-//            List<Integer> row = new ArrayList<>();
-//            int key = 0;
-//            for(int j = 0; j < score[i].length; j++){
-//                key = score[i][j];
-//                row.add(key);
-//            }
-//            table.put(key, row);
-//        }
-//        System.out.println(table);
-//        return null;
-//    }
+    public static void main(String[] args) {
+        new Solution().sortTheStudents(new int[][]{{10,6,9,1},{7,5,11,2},{4,8,3,15}}, 2);
+    }
+
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Map<Integer, List<Integer>> table = new HashMap<>();
+        for(int i = 0; i < score.length; i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < score[i].length; j++) {
+                row.add(score[i][j]);
+            }
+            table.put(score[i][k], row);
+        }
+        //System.out.println(table);
+        List<List<Integer>> result = new ArrayList<>();
+        table.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).forEach(e -> result.add(e.getValue()));
+        //System.out.println(result);
+        List<Integer>  matrixList = result.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        int index = 0;
+        for(int i = 0; i < score.length; i++){
+            for(int j = 0; j < score[i].length; j++) {
+                score[i][j] = matrixList.get(index);
+                index++;
+            }
+        }
+        //System.out.println(Arrays.deepToString(score));
+        return score;
+    }
 }
