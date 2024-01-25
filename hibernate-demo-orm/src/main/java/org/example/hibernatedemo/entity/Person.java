98402b7e2c4a4ea37800757129c9729768fa351b
Anton Lysenko <anton.lys96@gmail.com>
2024-01-25 18:07:07 +0200

    hibernate

--- hibernate-demo-orm/src/main/java/org/example/hibernatedemo/entity/Person.java
+++ hibernate-demo-orm/src/main/java/org/example/hibernatedemo/entity/Person.java
@@ -1,19 +1,42 @@
 package org.example.hibernatedemo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.example.hibernatedemo.orm.annotation.Column;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name = "person")
+//@Table(name = "person")
+@org.example.hibernatedemo.orm.annotation.Table(name = "person")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    //@Column(name = "first_name")
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
+    //@Column(name = "last_name")
     private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
