2ad7bf4cd00d8464df092cda8483c556ac892ee2
AntonLysenko <anton.lys96@gmail.com>
2024-03-01 06:39:16 -0500

    addByDateAndByAsinParent

--- spring-jwt/src/main/java/com/example/springjwt/controller/MainController.java
+++ spring-jwt/src/main/java/com/example/springjwt/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.springjwt.controller;
 
 import com.mongodb.client.DistinctIterable;
 import com.mongodb.client.FindIterable;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.BsonDocument;
 import org.bson.BsonNull;
@@ -11,7 +12,10 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -38,7 +42,33 @@ public class MainController {
         DistinctIterable<Document> findIterable = mongoTemplate.getCollection("test_report")
                 .distinct("salesAndTrafficByAsin", Document.class);
         for(Document doc : findIterable) documents.add(doc);
+        for(Document doc : findIterable) log.info(doc.getString("parentAsin"));
         return documents;
+    }
+
+    @GetMapping("/salesAndTrafficByAsin/{parentAsin}")
+    @Cacheable("getSalesAndTrafficByAsinParent")
+    public Document getSalesAndTrafficByAsinParent(@PathVariable("parentAsin") String parentAsin){
+        Document document = new Document();
+        DistinctIterable<Document> findIterable = mongoTemplate.getCollection("test_report")
+                .distinct("salesAndTrafficByAsin", Document.class);
+        for(Document doc : findIterable) {
+            if(doc.getString("parentAsin").equals(parentAsin)) document = doc;
+        }
+        return document;
+    }
+
+
+    @GetMapping("/salesAndTrafficByDate/{date}")
+    @Cacheable("getSalesAndTrafficByDateForDate")
+    public Document getSalesAndTrafficByDateForDate(@PathVariable("date") String date){
+        Document document = new Document();
+        DistinctIterable<Document> findIterable = mongoTemplate.getCollection("test_report")
+                .distinct("salesAndTrafficByDate", Document.class);
+        for(Document doc : findIterable) {
+            if(doc.getString("date").equals(date)) document = doc;
+        }
+        return document;
     }
 
     @GetMapping("/salesAndTrafficByDate")
@@ -53,9 +83,13 @@ public class MainController {
 
     @GetMapping("/resetCache")
     @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
-    @CacheEvict(cacheNames = {"getSalesAndTrafficByAsin", "getSalesAndTrafficByDate"})
+    @PostConstruct
+    @CacheEvict(cacheNames = {"getSalesAndTrafficByAsin",
+            "getSalesAndTrafficByDate",
+            "getSalesAndTrafficByAsinParent", 
+            "getSalesAndTrafficByDateForDate"})
     public void resetCache(){
-        log.debug("Reset cache");
+        log.info("Reset cache");
     }
 
     @GetMapping("/admin")
