1fa8463a4bcab998049867fa04a3db83284a7024
AntonLysenko <anton.lys96@gmail.com>
2024-04-15 03:18:45 -0400

    jax-rs

---
+++ jax-rs/src/main/java/org/example/jaxrs/HelloResource.java
@@ -0,0 +1,29 @@
+package org.example.jaxrs;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloResource {
+    @GET
+    @Path("/1")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        return Response.status(Response.Status.OK)
+                .entity(new Message("Hello, World!"))
+                .build();
+    }
+
+    @GET
+    @Path("/2")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response bye() {
+        return Response.status(Response.Status.OK)
+                .entity(new Message("Bye, World!"))
+                .build();
+    }
+}
