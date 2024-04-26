9dccac512898a3fc15c30c1bbcdb083905bf29f8
AntonLysenko <anton.lys96@gmail.com>
2024-04-26 10:18:32 -0400

    send file & text email

--- sendmail/src/main/java/org/example/Main.java
+++ sendmail/src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class Main {
 
-    private String emailAddressTo = "xxx@gmail.com";
+    private String emailAddressTo = "anton.lys96@gmail.com";
     private String msgSubject;
     private String msgText;
 
 
     public static void main(String[] args) {
-        new Main().createAndSendEmail("xxx@gmail.com", "subject", "Text");
+        new Main().createAndSendEmail("anton.lys96@gmail.com", "subject", "Text");
     }
 
 
@@ -30,6 +35,7 @@ public class Main {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
+//        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.port", "587");
         Session session = Session.getDefaultInstance(props, new Authenticator() {
             @Override
@@ -40,10 +46,26 @@ public class Main {
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(emailAddressTo));
-            message.setContent(msgText, "text/html");
 
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAddressTo));
+            BodyPart messageBodyPart = new MimeBodyPart();
+            Multipart multipart = new MimeMultipart();
+
+            messageBodyPart.setContent(msgSubject, "text/html");
+            multipart.addBodyPart(messageBodyPart);
+            messageBodyPart = new MimeBodyPart();
+//            String fileName = "/home/user/Documents/SimpleExamples/sendmail/file.txt";
+            String fileName = "/home/user/Documents/SimpleExamples/sendmail/2024-04-25_08-16.png";
+            DataSource source = new FileDataSource(fileName);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+
+
+            message.setFrom(new InternetAddress(emailAddressTo));
+//            message.setContent(msgText, "text/html");
+            message.setContent(multipart);
+
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("anton.lysenko.info@gmail.com"));
 
             message.setSubject("Hello");
             Transport.send(message);
--- springmail/src/main/java/com/example/springmail/service/MailService.java
+++ springmail/src/main/java/com/example/springmail/service/MailService.java
@@ -1,28 +1,67 @@
 package com.example.springmail.service;
 
 import com.example.springmail.model.Loggable;
+import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
+import jakarta.activation.FileDataSource;
+import jakarta.mail.BodyPart;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class MailService {
 
     private JavaMailSender javaMailSender;
 
-    @Autowired
+
     public MailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     @Loggable
-    public SimpleMailMessage sendMailMessage(String to, String subject, String text){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
-        return message;
+    public MimeMessage sendMailMessage(String to, String subject, String text) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessage mimeMessage1 = null;
+        try {
+        MimeMessageHelper helper = new MimeMessageHelper(javaMailSender.createMimeMessage(), true);
+            helper.addAttachment("application.properties", new File("/home/user/Documents/SimpleExamples/springmail/src/main/resources/application.properties"));
+            helper.addTo("anton.lysenko.info@gmail.com");
+            helper.setText("text");
+            mimeMessage1 = helper.getMimeMessage();
+//            Multipart multipart = new MimeMultipart();
+//            mimeMessage.setRecipients(Message.RecipientType.TO, "anton.lysenko.info@gmail.com");
+//            DataSource dataSource = new FileDataSource("/home/user/Documents/SimpleExamples/springmail/src/main/resources/application.properties");
+//            BodyPart bodyPart = new MimeBodyPart();
+//            bodyPart.setDataHandler(new DataHandler(dataSource));
+//            bodyPart.setFileName("application.properties");
+//            multipart.addBodyPart(bodyPart);
+//            bodyPart = new MimeBodyPart();
+//            bodyPart.setText("properties");
+//            multipart.addBodyPart(bodyPart);
+//            mimeMessage.setContent(multipart);
+            javaMailSender.send(mimeMessage1);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(text);
+//        javaMailSender.send(message);
+        return mimeMessage1;
     }
 }
