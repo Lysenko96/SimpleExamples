75d32093c3c670b9fadaf670acf2d8e26b4591f2
AntonLysenko <anton.lys96@gmail.com>
2024-05-20 04:17:45 -0400

    review

--- window_function.sql
+++ window_function.sql
@@ -13,15 +13,26 @@ select * from barter;
 select name, price,
        sum(price)
        filter ( where price > 500)
-           over (partition by name, price ORDER BY name, price DESC range unbounded preceding) as sum_grade,
-       avg(price) over (partition by name) as avg_grade,
-       count(price) over (partition by name) as count_grade,
-       min(price) over (partition by name) as min_grade,
-       max(price) over (partition by name) as max_grade
+           over (partition by name, price ORDER BY name, price DESC RANGE UNBOUNDED PRECEDING) as sum_price,
+       avg(price) over (partition by name) as avg_price,
+       count(price) over (partition by name) as count_price,
+       min(price) over (partition by name) as min_price,
+       max(price) over (partition by name) as max_price
 from barter;
 
 select name, price,
        row_number() over (partition by name order by price desc),
        rank() over (partition by name order by price desc),
        dense_rank() over (partition by name order by price desc)
+from barter;
+
+
+select name, price,
+       lag(price) over (order by name) as previous_price,
+       lead(price) over (order by name) as next_price
+from barter;
+
+select name, price,
+       first_value(price) over (order by name ) as first_price,
+       last_value(price) over (order by name RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as last_price
 from barter;
