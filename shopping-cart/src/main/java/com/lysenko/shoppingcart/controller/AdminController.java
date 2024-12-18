66d9d44193c3dbe49cc25ff830eb48d0cffa3a4b
John Doe <anton.lysenko.info@gmail.com>
2024-12-19 01:38:41 +0200

    admin dashboard

--- shopping-cart/src/main/java/com/lysenko/shoppingcart/controller/AdminController.java
+++ shopping-cart/src/main/java/com/lysenko/shoppingcart/controller/AdminController.java
@@ -1,7 +1,25 @@
 package com.lysenko.shoppingcart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/shopping-cart/admin")
 public class AdminController {
+
+    @GetMapping("/")
+    public String index() {
+        return "admin/index";
+    }
+
+    @GetMapping("/add-product")
+    public String adminAddProduct() {
+        return "admin/add_product";
+    }
+
+    @GetMapping("/category")
+    public String category() {
+        return "admin/category";
+    }
 }
