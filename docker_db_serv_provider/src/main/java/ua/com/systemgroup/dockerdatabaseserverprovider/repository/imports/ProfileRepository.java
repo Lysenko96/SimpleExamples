3c8533d22bc9cd2bf109f671f99c50d20bb21e7b
antonLysenko <Anton.Lysenko@systemgroup.com.ua>
2025-03-10 13:57:38 +0200

    del

--- docker_db_serv_provider/src/main/java/ua/com/systemgroup/dockerdatabaseserverprovider/repository/imports/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.com.systemgroup.dockerdatabaseserverprovider.repository.imports;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.com.systemgroup.dockerdatabaseserverprovider.model.imports.Profile;
-import ua.com.systemgroup.dockerdatabaseserverprovider.model.imports.pkey.ProfilePk;
-
-public interface ProfileRepository extends JpaRepository<Profile, ProfilePk> {
-}
