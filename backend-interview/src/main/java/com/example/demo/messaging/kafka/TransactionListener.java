7eb4f797e75f5e3e5a5d2c8dd70bc9915b0f4439
antonLysenko <Anton.Lysenko@systemgroup.com.ua>
2026-07-11 20:13:35 +0300

    fixed kafka

--- backend-interview/src/main/java/com/example/demo/messaging/kafka/TransactionListener.java
+++ backend-interview/src/main/java/com/example/demo/messaging/kafka/TransactionListener.java
@@ -7,38 +7,26 @@ import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class TransactionListener {
 
     // TODO
 
-//    private final ApplicationEventPublisher applicationEventPublisher;
-//
-//    public TransactionListener(ApplicationEventPublisher applicationEventPublisher) {
-//        this.applicationEventPublisher = applicationEventPublisher;
-//    }
-//
-//    public void publishCreated(Long transactionId) {
-//        applicationEventPublisher.publishEvent(new Event());
-//    }
-//
-//    public void publishUpdated(Long transactionId) {
-//        applicationEventPublisher.publishEvent(new Event());
-//    }
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-//    private final KafkaTemplate<String, Transaction> kafkaTemplate;
-//
-//    public TransactionListener(KafkaTemplate<String, Transaction> kafkaTemplate) {
-//        this.kafkaTemplate = kafkaTemplate;
-//    }
-//
-//    @EventListener
-//    public void onTransactionCreate(Transaction transactionCreate) {
-//        kafkaTemplate.send("transactionCreate", transactionCreate);
-//    }
-//
-//    @EventListener
-//    public void onTransactionUpdate(Transaction transactionUpdate) {
-//        kafkaTemplate.send("transactionUpdate", transactionUpdate);
-//    }
+    public TransactionListener(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @EventListener
+    public void onTransactionCreate(Transaction transactionCreate) {
+        kafkaTemplate.send("transactionCreate", UUID.randomUUID().toString(), transactionCreate.toString());
+    }
+
+    @EventListener
+    public void onTransactionUpdate(Transaction transactionUpdate) {
+        kafkaTemplate.send("transactionUpdate", UUID.randomUUID().toString(), transactionUpdate.toString());
+    }
 }
