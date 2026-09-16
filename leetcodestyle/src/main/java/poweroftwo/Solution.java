ef3165d61dd4ae84616c712b3d51ae6b5ad3a9fe
antonlysenko <anton.lysenko.send@gmail.com>
2026-09-16 23:53:15 +0300

    add ez tasks

---
+++ leetcodestyle/src/main/java/poweroftwo/Solution.java
@@ -0,0 +1,13 @@
+package poweroftwo;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.isPowerOfTwo(1);
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && n == Integer.highestOneBit(n);
+    }
+}
