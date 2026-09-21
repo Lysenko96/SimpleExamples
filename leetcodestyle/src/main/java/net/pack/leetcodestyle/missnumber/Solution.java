56625a33dc3b5ea795a44f179a1eb3270a8c923b
antonlysenko <anton.lysenko.send@gmail.com>
2026-09-22 00:31:22 +0300

    add ez tasks

---
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/missnumber/Solution.java
@@ -0,0 +1,27 @@
+package net.pack.leetcodestyle.missnumber;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.missingNumber(new int[]{0, 1}));
+    }
+
+    public int missingNumber(int[] nums) {
+        int j = 0;
+        nums = Arrays.stream(nums).sorted().toArray();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != j) {
+                return j;
+            }
+            j++;
+        }
+        return nums.length;
+    }
+}
