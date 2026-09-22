9b1778cdf1d43a644213427a0ae01a3092837c51
antonlysenko <anton.lysenko.send@gmail.com>
2026-09-22 20:44:02 +0300

    add ez tasks

---
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/isomorphic/Solution.java
@@ -0,0 +1,33 @@
+package net.pack.leetcodestyle.isomorphic;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isIsomorphic("egg", "a12"));
+        System.out.println(s.isIsomorphic("paper", "title"));
+        System.out.println(s.isIsomorphic("bbbaaaba", "aaabbbba"));
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+        String[] arrS = s.split("");
+        String[] arrT = t.split("");
+        String[] res = new String[arrS.length];
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < arrS.length; i++) {
+            if (!map.containsKey(arrS[i]) && !map.containsValue(arrT[i])) {
+                map.put(arrS[i], arrT[i]);
+            }
+        }
+        for (int i = 0; i < arrS.length; i++) {
+            res[i] = map.get(arrS[i]);
+        }
+        return String.join("", res).equals(t);
+    }
+
+}
--- leetcodestyle/src/main/java/net/pack/leetcodestyle/missnumber/Solution.java
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/missnumber/Solution.java
@@ -1,10 +1,6 @@
 package net.pack.leetcodestyle.missnumber;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Solution {
 
