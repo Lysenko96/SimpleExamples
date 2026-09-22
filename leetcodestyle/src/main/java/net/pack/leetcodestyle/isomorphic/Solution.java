fd64c08208213a69000c6565135ca99b6c431cf1
antonlysenko <anton.lysenko.send@gmail.com>
2026-09-22 20:44:30 +0300

    add ez tasks

--- leetcodestyle/src/main/java/net/pack/leetcodestyle/isomorphic/Solution.java
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/isomorphic/Solution.java
@@ -1,9 +1,7 @@
 package net.pack.leetcodestyle.isomorphic;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Solution {
 
