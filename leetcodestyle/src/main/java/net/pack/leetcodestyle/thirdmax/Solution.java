3b6791a87e78af9e1a696f29b9dd3ba662f43f1e
antonlysenko <anton.lysenko.send@gmail.com>
2026-09-24 00:39:17 +0300

    add ez tasks

---
+++ leetcodestyle/src/main/java/net/pack/leetcodestyle/thirdmax/Solution.java
@@ -0,0 +1,28 @@
+package net.pack.leetcodestyle.thirdmax;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.thirdMax(new int[]{1, 2}));
+        System.out.println(s.thirdMax(new int[]{1, 1, 3, 3, 5, 5, 4, 4, 4}));
+    }
+
+    public int thirdMax(int[] nums) {
+        Set<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        if (numbers.size() <= 2) {
+            return Collections.max(numbers);
+        }
+        int firstMax = Collections.max(numbers);
+        numbers.remove(firstMax);
+        int secondMax = Collections.max(numbers);
+        numbers.remove(secondMax);
+        return Collections.max(numbers);
+    }
+}
